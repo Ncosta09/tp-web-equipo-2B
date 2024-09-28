1cf591ff8f4790d778214ab7f6241544d2d5b6b1
AgustinDeDonato <agusdedonato@hotmail.com>
2024-09-28 13:38:45 -0300

    Creacion Clases + ASPX Registro

---
+++ Dominios/Artículo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using 
+
+namespace Dominios
+{
+    public class Artículo
+    {
+        public int Id { get; set; }
+        public string Código { get; set; }
+        public string Nombre { get; set; }
+        public string Descripción { get; set; }
+
+        public Imagen imagen { get; set; }
+
+        public Categoria categoria { get; set; }
+
+        public Marca marca { get; set; }
+
+
+
+    }
+}
